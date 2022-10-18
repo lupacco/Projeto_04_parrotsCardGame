42878ba0f004ababd5de8037f29e1a217f1d8059
Lucas <pagottolc@gmail.com>
2022-10-18 03:04:25 -0300

    Virando cartas e requisitando qtd de cartas OK

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 function askCardsQuantity(){
     let cardsQuantity = prompt("Com quantas cartas você deseja jogar?")
-
+    if(cardsQuantity < 4 || cardsQuantity > 14 || cardsQuantity%2 != 0){
+        alert("Você deve escolher um número par entre 4 e 14")
+        return askCardsQuantity()
+    }
     return cardsQuantity
+}
+
+function flipCard(card){
+    card.querySelector('.back').classList.toggle('hide')
+    card.querySelector('.front').classList.toggle('hide')
+    
 }
